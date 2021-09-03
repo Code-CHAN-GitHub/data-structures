d5287d6d9bd85eeba767be50f06319ba28b15fe9
chenjunjie <1648192188@qq.com>
2021-09-03 15:07:21 +0800

    heap add

--- heap/Heap.c
+++ heap/Heap.c
@@ -9,3 +9,47 @@ struct HeapStruct {
     int capacity;
     int *data;
 };
+
+int EMPTY_DATA[];
+
+Heap createHeap() {
+    Heap heap = (Heap) malloc(sizeof(struct HeapStruct));
+    heap->size = heap->capacity = 0;
+    heap->data = EMPTY_DATA;
+    return heap;
+}
+
+void heapGrow(Heap heap, int minCapacity) {
+    int oldCapacity = heap->capacity;
+    int newCapacity = oldCapacity + ((oldCapacity < 64) ?
+                                     (oldCapacity + 2) :
+                                     (oldCapacity >> 1));
+    if (newCapacity < minCapacity)
+        newCapacity = minCapacity;
+    int *oldData = heap->data;
+    int *newData = (int *) malloc(sizeof(int ) * newCapacity);
+    arrayCopy(oldData, 1, newData, 1, oldCapacity);
+    heap->capacity = newCapacity;
+    heap->data = newData;
+    if (heap->size != 0)
+        free(oldData);
+}
+
+void heapAdd(Heap heap, int val) {
+    if (heap->size == heap->capacity)
+        heapGrow(heap, heap->capacity + 2);
+
+    int i;
+    for (i = ++heap->size; i > 1 && heap->data[i / 2] > val; i /= 2)
+        heap->data[i] = heap->data[i / 2];
+    heap->data[i] = val;
+}
+
+void printHeap(Heap heap) {
+    printf("[");
+    for (int i = 1; i < heap->size; i++)
+        printf("%d, ", heap->data[i]);
+    if (heap->size > 0)
+        printf("%d", heap->data[heap->size]);
+    printf("]\n");
+}
--- heap/Heap.h
+++ heap/Heap.h
@@ -7,10 +7,22 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "../util/Arrays.h"
 
 struct HeapStruct;
 typedef struct HeapStruct *Heap;
 
+/**
+ * 返回一个空的堆
+ * @return
+ */
+Heap createHeap();
 
+/**
+ * 将元素压入堆中
+ */
+void heapAdd(Heap heap, int val);
+
+void printHeap(Heap heap);
 
 #endif //DATA_STRUCTURES_HEAP_H
