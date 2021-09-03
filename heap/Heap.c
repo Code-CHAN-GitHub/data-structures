8bf62d0130bea931b2ea5000b561fedd27f9016a
chenjunjie <1648192188@qq.com>
2021-09-03 14:22:38 +0800

    heap 定义

---
+++ heap/Heap.c
@@ -0,0 +1,11 @@
+//
+// Created by chen on 2021/9/3.
+//
+
+#include "Heap.h"
+
+struct HeapStruct {
+    int size;
+    int capacity;
+    int *data;
+};
---
+++ heap/Heap.h
@@ -0,0 +1,16 @@
+/**
+ * 堆
+ */
+
+#ifndef DATA_STRUCTURES_HEAP_H
+#define DATA_STRUCTURES_HEAP_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct HeapStruct;
+typedef struct HeapStruct *Heap;
+
+
+
+#endif //DATA_STRUCTURES_HEAP_H
