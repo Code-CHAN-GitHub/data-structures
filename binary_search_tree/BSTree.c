a1c0fd511417f2f9019ef429076ef0384692da47
chenjunjie <1648192188@qq.com>
2021-09-04 09:40:27 +0800

    BSTree 定义

---
+++ binary_search_tree/BSTree.c
@@ -0,0 +1,17 @@
+//
+// Created by chen on 2021/9/4.
+//
+
+#include "BSTree.h"
+
+struct TreeNodeStruct {
+    ElementType val;
+    TreeNode left;
+    TreeNode right;
+};
+
+struct BSTreeStruct {
+    int size;
+    TreeNode root;
+};
+
---
+++ binary_search_tree/BSTree.h
@@ -0,0 +1,21 @@
+//
+// Created by chen on 2021/9/4.
+//
+
+#ifndef DATA_STRUCTURES_BSTREE_H
+#define DATA_STRUCTURES_BSTREE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int ElementType;
+
+struct TreeNodeStruct;
+typedef struct TreeNodeStruct *TreeNode;
+
+struct BSTreeStruct;
+typedef struct BSTreeStruct *BSTree;
+
+
+
+#endif //DATA_STRUCTURES_BSTREE_H
