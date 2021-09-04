297c71e0054f2ed5c6c3ae77395a9e5221dbd16f
chenjunjie <1648192188@qq.com>
2021-09-04 14:42:14 +0800

    Math 添加宏 min、max

---
+++ util/Math.c
@@ -0,0 +1,4 @@
+//
+// Created by chen on 2021/9/4.
+//
+
---
+++ util/Math.h
@@ -0,0 +1,11 @@
+//
+// Created by chen on 2021/9/4.
+//
+
+#ifndef DATA_STRUCTURES_MATH_H
+#define DATA_STRUCTURES_MATH_H
+
+#define max(x, y) ((x > y) ? (x) : (y))
+#define min(x, y) ((x < y) ? (x) : (y))
+
+#endif //DATA_STRUCTURES_MATH_H
